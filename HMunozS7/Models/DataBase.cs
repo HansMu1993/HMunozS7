3374272a3515f0a72988525841c44c9162a31863
HansMu1993 <hh_hansmaster@hotmail.com>
2023-05-23 04:59:34 -0500

    Agregar archivos de proyecto.

---
+++ HMunozS7/Models/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMunozS7.Models
+{
+
+    public interface DataBase
+    {
+        SQLiteAsyncConnection GetConnection(); //defino el metodo de conexion
+    }
+}
---
+++ HMunozS7/Vistas/Registro.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMunozS7.Models;
+using SQLite;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HMunozS7.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        private SQLiteAsyncConnection conexion;
+        public Registro()
+        {
+            InitializeComponent();
+            conexion = DependencyService.Get<DataBase>().GetConnection();
+        }
+
+        private void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var datos = new Estudiante
+                {
+                    Nombre = txtNombre.Text,
+                    Usuario = txtUsuario.Text,
+                    Contrasena = txtContrasena.Text
+                };
+                conexion.InsertAsync(datos);
+
+                txtNombre.Text = "";
+                txtUsuario.Text = "";
+                txtContrasena.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+        }
+    }
+}
